fb9379173a89b0653ab2c812ad0d89042c3e9395
Mateusz Dropinski <mateuszdropinski5@gmail.com>
2018-02-28 21:28:06 +0100

    Pokemon data changed

--- src/components/Card_section.js
+++ src/components/Card_section.js
@@ -3,11 +3,21 @@ import styled from 'styled-components';
 
 const CardSection = (props) =>
 {
+    const InfoWrapper = styled.p`
+        width:100%;
+        display:flex;
+        justify-content:space-between;
+        font-size:1rem;
+        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+        padding:5px;
+        margin:5px 0px 5px 0px;
+    `
+    
     return (
-        <article>
+        <InfoWrapper>
             <span>{props.name}</span>
             <span>{props.value}</span>
-        </article>
+        </InfoWrapper>
     )
 }
 
--- src/components/Pokemon_card.js
+++ src/components/Pokemon_card.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { CardSection, PokemonType } from './';
+import styled from 'styled-components';
+import Types from '../reducers/types';
 
 const PokemonCard = (props) =>
 {
@@ -12,14 +14,41 @@ const PokemonCard = (props) =>
         return  <PokemonType key={type} type={type} />;
     }
     
+    let cardColor;
+    
+    Types.map(type => {
+        if(Object.keys(type)[0] == types[0])
+        {
+            cardColor = type[types[0]].backgroundColor;
+        }
+    });
+    
+    const Card = styled.article`
+        box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+        width:200px;
+        display:flex;
+        justify-content:space-around;
+        flex-wrap:wrap;
+        margin:0px 20px 20px 20px;
+        padding:20px;
+    `;
+    
+    const Image = styled.img`
+        width:100%;
+        height:auto;
+        align-self:center;
+        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+        margin-bottom:5px;
+    `
+    
     return(
-        <article>
-            <img src={image} alt={name + " image"} />
+        <Card>
+            <Image src={image} alt={name + " image"} />
             <CardSection name="name" value={name}></CardSection>
             <CardSection name="height" value={height + " m"}></CardSection>
             <CardSection name="weight" value={weight + " kg"}></CardSection>
             {types.map(type => renderTypes(type))}
-        </article>
+        </Card>
     )
 }
 
